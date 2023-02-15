eb80f43597abe1a2b95ae10ed986cef283245ef7
Heiff <87312458+Heiff@users.noreply.github.com>
2023-02-16 03:22:08 +0500

    s

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors())
 
-app.use('/Products', productsRoutes)
+app.use('/', productsRoutes)
 app.listen(process.env.PORT,()=>console.log('succes 3000 PORT'))
 
 
